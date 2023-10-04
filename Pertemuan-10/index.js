5ef8af9e12c97aa162695dc13662a0596c1bd57b
Tiffany_lintang <146844347+Tiffanylintng02@users.noreply.github.com>
2023-10-04 23:23:13 +0800

    Add files via upload

---
+++ Pertemuan-10/index.js
@@ -0,0 +1,19 @@
+// Lintang, Tiffany
+// Parallel E
+
+import helloWorld from "./helloWorld.js";
+
+async function messages() {
+  const msg = await helloWorld();
+  console.log(msg);
+}
+
+messages();
+
+import ambilDataUser from "./ambilDataUser.js";
+
+ambilDataUser();
+
+import ambilDataUserAsync from "./ambilDataUserAsync.js";
+
+ambilDataUserAsync();
---
+++ Pertemuan-7/index.js
@@ -0,0 +1,27 @@
+let fulName = "john doe"
+let age = 33
+let address = "manado"
+
+// let kalimat = "hallo nama saya adalah " +
+// fulName + 
+// " umur saya " + age + " dan alamat saya " + address
+
+// console.log(kalimat)
+
+// let kalimat6 = `nama saya adalah ${fulName} umur saya adalah ${age}, dan alamat saya ${address}.`
+// console.log(kalimat6)
+
+// function sayGreaetings(name) {
+//     return `hallo ${name}`
+// }
+
+// console.log(sayGreaetings);
+
+// const sayGreaetings2 = (nama) => `hallo ${nama}`
+// console.log(sayGreaetings2 ("john"));
+
+// let output = (() => `hello`())
+
+// const sayGreaetings5 = (fulName = "john doe") => {
+//     console.log(`hallo ${fulName}`)
+// }s
